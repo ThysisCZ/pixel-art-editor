6e6f4d121f9df5ead17e06f27bb8b412adde6d87
Jiří Kerner <jirikernerjr@gmail.com>
2026-06-25 01:35:47 +0200

    Undo drawing

--- main.c
+++ main.c
@@ -9,6 +9,8 @@
 #define CELL_COUNT CANVAS_WIDTH *CANVAS_HEIGHT
 #define COLOR_COUNT 21
 #define COLOR_SIZE 75
+#define MAX_UNDO_STROKES 1000
+#define MAX_STROKE_CELLS CELL_COUNT
 
 Color colors[COLOR_COUNT] = {DARKGRAY,
                              MAROON,
@@ -40,6 +42,21 @@ typedef struct
     float pos_y;
     Color color;
 } Pixel;
+
+typedef struct
+{
+    int cell_count;
+    int cells[MAX_STROKE_CELLS];
+    Color prev_colors[MAX_STROKE_CELLS];
+} UndoStroke;
+
+UndoStroke undo_strokes[MAX_UNDO_STROKES];
+int undo_stroke_count = 0;
+
+int drawing_stroke = 0;
+int current_stroke_cell_count = 0;
+int current_stroke_cells[MAX_STROKE_CELLS];
+Color current_stroke_prev_colors[MAX_STROKE_CELLS];
 
 void draw_canvas(Pixel *pixels)
 {
@@ -174,11 +191,11 @@ void cell_hover()
     }
 }
 
-void draw_pixel(Pixel *pixels)
+int get_hovered_cell_index(void)
 {
     Vector2 mouse = GetMousePosition();
-    int mx = mouse.x;
-    int my = mouse.y;
+    int mx = (int)mouse.x;
+    int my = (int)mouse.y;
 
     for (int i = 0; i < CELL_COUNT; i++)
     {
@@ -191,11 +208,71 @@ void draw_pixel(Pixel *pixels)
         if (mx >= cx && mx < cx + CELL_SIZE &&
             my >= cy && my < cy + CELL_SIZE)
         {
-            if (select_index != -1 && IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+int already_in_current_stroke(int idx)
+{
+    for (int i = 0; i < current_stroke_cell_count; i++)
+    {
+        if (current_stroke_cells[i] == idx)
+        {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+void draw_pixel(Pixel *pixels)
+{
+    if (select_index == -1)
+    {
+        return;
+    }
+
+    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+    {
+        drawing_stroke = 1;
+        current_stroke_cell_count = 0;
+    }
+
+    if (drawing_stroke && IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+    {
+        int idx = get_hovered_cell_index();
+
+        if (idx != -1 && !already_in_current_stroke(idx))
+        {
+            current_stroke_cells[current_stroke_cell_count] = idx;
+            current_stroke_prev_colors[current_stroke_cell_count] = pixels[idx].color;
+            current_stroke_cell_count++;
+
+            pixels[idx].color = current_color;
+        }
+    }
+
+    if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT))
+    {
+        if (current_stroke_cell_count > 0)
+        {
+            if (undo_stroke_count < MAX_UNDO_STROKES)
             {
-                pixels[i].color = current_color;
+                UndoStroke *s = &undo_strokes[undo_stroke_count++];
+                s->cell_count = current_stroke_cell_count;
+
+                for (int i = 0; i < current_stroke_cell_count; i++)
+                {
+                    s->cells[i] = current_stroke_cells[i];
+                    s->prev_colors[i] = current_stroke_prev_colors[i];
+                }
             }
         }
+
+        drawing_stroke = 0;
     }
 }
 
@@ -226,11 +303,25 @@ void erase_pixel(Pixel *pixels)
     }
 }
 
+void undo(Pixel *pixels)
+{
+    if (IsKeyPressed(KEY_Y) && undo_stroke_count > 0)
+    {
+        UndoStroke *s = &undo_strokes[--undo_stroke_count];
+
+        for (int i = 0; i < s->cell_count; i++)
+        {
+            pixels[s->cells[i]].color = s->prev_colors[i];
+        }
+    }
+}
+
 int main()
 {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Pixel Art Editor");
 
     Pixel pixels[CELL_COUNT];
+    Pixel pixels_prev[CELL_COUNT];
 
     for (int i = 0; i < CELL_COUNT; i++)
     {
@@ -243,6 +334,10 @@ int main()
         pixels[i].pos_x = pos_x;
         pixels[i].pos_y = pos_y;
         pixels[i].color = WHITE;
+
+        pixels_prev[i].pos_x = pos_x;
+        pixels_prev[i].pos_y = pos_y;
+        pixels_prev[i].color = WHITE;
     }
 
     while (!WindowShouldClose())
@@ -258,6 +353,8 @@ int main()
         cell_hover();
         draw_pixel(&pixels[0]);
         erase_pixel(&pixels[0]);
+
+        undo(&pixels[0]);
 
         EndDrawing();
     }
