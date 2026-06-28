807d0868a6918fe3211b37fac733bdd03816cebf
Jiří Kerner <jirikernerjr@gmail.com>
2026-06-29 01:06:12 +0200

    Improved fill algorithm

--- main.c
+++ main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <raylib.h>
+#include <math.h>
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 900
 #define CELL_SIZE 25
 #define CANVAS_WIDTH 30
 #define CANVAS_HEIGHT 20
-#define CELL_COUNT CANVAS_WIDTH *CANVAS_HEIGHT
+#define CELL_COUNT (CANVAS_WIDTH * CANVAS_HEIGHT)
 #define COLOR_COUNT 21
 #define COLOR_SIZE 75
 #define MAX_STROKES 1000
-#define MAX_STROKE_CELLS CELL_COUNT
+#define MAX_DEAD_ENDS (12 * CELL_COUNT)
 
 Color colors[COLOR_COUNT] = {DARKGRAY,
                              MAROON,
@@ -46,9 +47,9 @@ typedef struct
 typedef struct
 {
     int cell_count;
-    int cells[MAX_STROKE_CELLS];
-    Color prev_colors[MAX_STROKE_CELLS];
-    Color next_colors[MAX_STROKE_CELLS];
+    int cells[CELL_COUNT];
+    Color prev_colors[CELL_COUNT];
+    Color next_colors[CELL_COUNT];
 } Stroke;
 
 Stroke strokes[MAX_STROKES];
@@ -57,12 +58,17 @@ int strokes_total = 0;
 
 bool drawing_stroke = false;
 int current_stroke_cell_count = 0;
-int current_stroke_cells[MAX_STROKE_CELLS];
-Color current_stroke_prev_colors[MAX_STROKE_CELLS];
-Color current_stroke_next_colors[MAX_STROKE_CELLS];
+int current_stroke_cells[CELL_COUNT];
+Color current_stroke_prev_colors[CELL_COUNT];
+Color current_stroke_next_colors[CELL_COUNT];
 
 bool fill_mode = false;
-Color color_before_draw = WHITE;
+bool fill_active = false;
+Color fill_source_color = WHITE;
+int fill_current_index = -1;
+int dead_end_count = 0;
+int current_fill_cell_count = 0;
+int current_fill_cells[CELL_COUNT];
 
 void draw_canvas(Pixel *pixels)
 {
@@ -190,10 +196,21 @@ void cell_hover()
         if (mx >= cx && mx < cx + CELL_SIZE &&
             my >= cy && my < cy + CELL_SIZE)
         {
-            if (select_index != -1)
+            if (!fill_mode)
             {
-                DrawRectangle(cx, cy, CELL_SIZE, CELL_SIZE, selected_color);
-                DrawRectangleLines(cx, cy, CELL_SIZE, CELL_SIZE, WHITE);
+                if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT))
+                {
+                    DrawRectangle(cx, cy, CELL_SIZE, CELL_SIZE, WHITE);
+                    DrawRectangleLines(cx, cy, CELL_SIZE, CELL_SIZE, WHITE);
+                }
+                else
+                {
+                    if (select_index != -1)
+                    {
+                        DrawRectangle(cx, cy, CELL_SIZE, CELL_SIZE, selected_color);
+                        DrawRectangleLines(cx, cy, CELL_SIZE, CELL_SIZE, WHITE);
+                    }
+                }
             }
         }
     }
@@ -256,7 +273,6 @@ void draw_pixel(Pixel *pixels)
             current_stroke_next_colors[current_stroke_cell_count] = selected_color;
             current_stroke_cell_count++;
 
-            color_before_draw = pixels[current_cell_index].color;
             pixels[current_cell_index].color = selected_color;
         }
     }
@@ -308,7 +324,6 @@ void erase_pixel(Pixel *pixels)
             current_stroke_next_colors[current_stroke_cell_count] = WHITE;
             current_stroke_cell_count++;
 
-            color_before_draw = pixels[current_cell_index].color;
             pixels[current_cell_index].color = WHITE;
         }
     }
@@ -373,9 +388,66 @@ static bool colors_equal(Color c1, Color c2)
             c1.b == c2.b);
 }
 
+static int generate_next_index(int current_index)
+{
+    int right_index = current_index + 1;
+    int bottom_index = current_index + CANVAS_WIDTH;
+    int left_index = current_index - 1;
+    int top_index = current_index - CANVAS_WIDTH;
+
+    int random_index = GetRandomValue(1, 4);
+    int next_index = current_index;
+
+    switch (random_index)
+    {
+    case 1:
+        if (right_index < CELL_COUNT)
+        {
+            next_index = right_index;
+            break;
+        }
+    case 2:
+        if (bottom_index < CELL_COUNT)
+        {
+            next_index = bottom_index;
+            break;
+        }
+    case 3:
+        if (left_index >= 0)
+        {
+            next_index = left_index;
+            break;
+        }
+    case 4:
+        if (top_index >= 0)
+        {
+            next_index = top_index;
+            break;
+        }
+    }
+
+    return next_index;
+}
+
+static bool already_in_current_fill(int current_cell_index)
+{
+    for (int i = 0; i < CELL_COUNT; i++)
+    {
+        if (current_fill_cells[i] == current_cell_index)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
 void fill(Pixel *pixels)
 {
-    int current_cell_index = get_current_cell_index();
+    int start_index = get_current_cell_index();
+
+    float pos_x = pixels[start_index].pos_x;
+    float pos_y = pixels[start_index].pos_y;
 
     if (IsKeyPressed(KEY_B))
     {
@@ -389,35 +461,163 @@ void fill(Pixel *pixels)
         }
     }
 
-    if (fill_mode && current_cell_index != -1 && select_index != -1)
+    if (fill_mode && start_index != -1)
     {
-        float pos_x = pixels[current_cell_index].pos_x;
-        float pos_y = pixels[current_cell_index].pos_y;
-
-        DrawRectangle(pos_x, pos_y, CELL_SIZE, CELL_SIZE, BLACK);
-        DrawRectangle(pos_x + CELL_SIZE / 4, pos_y + CELL_SIZE / 4, CELL_SIZE / 2, CELL_SIZE / 2, selected_color);
-        DrawRectangleLines(pos_x, pos_y, CELL_SIZE, CELL_SIZE, WHITE);
-
         if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
         {
-            for (int i = 0; i < CELL_COUNT; i++)
-            {
-                if (colors_equal(pixels[i].color, color_before_draw))
-                {
-                    pixels[i].color = selected_color;
-                }
-            }
+            fill_source_color = pixels[start_index].color;
+            fill_active = true;
+
+            fill_current_index = start_index;
+            dead_end_count = 0;
+            current_fill_cell_count = 0;
         }
 
         if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT))
         {
-            for (int i = 0; i < CELL_COUNT; i++)
+            fill_source_color = pixels[start_index].color;
+            fill_active = true;
+
+            fill_current_index = start_index;
+            dead_end_count = 0;
+            current_fill_cell_count = 0;
+        }
+
+        if ((IsMouseButtonReleased(MOUSE_BUTTON_LEFT) ||
+             (IsMouseButtonReleased(MOUSE_BUTTON_RIGHT))) &&
+            fill_active)
+        {
+            fill_source_color = WHITE;
+            fill_active = false;
+
+            fill_current_index = -1;
+            dead_end_count = 0;
+        }
+
+        if (select_index != -1)
+        {
+            // Fill with palette colors
+            if (fill_active && IsMouseButtonDown(MOUSE_BUTTON_LEFT) && !colors_equal(fill_source_color, selected_color))
             {
-                if (colors_equal(pixels[i].color, color_before_draw))
+                while (dead_end_count < MAX_DEAD_ENDS)
                 {
-                    pixels[i].color = WHITE;
+                    int current_index = fill_current_index;
+                    int next_index = generate_next_index(current_index);
+
+                    int right_index = current_index + 1;
+                    int bottom_index = current_index + CANVAS_WIDTH;
+                    int left_index = current_index - 1;
+                    int top_index = current_index - CANVAS_WIDTH;
+
+                    if (colors_equal(pixels[current_index].color, fill_source_color))
+                    {
+                        pixels[current_index].color = selected_color;
+                        current_fill_cells[current_fill_cell_count] = current_index;
+                        current_fill_cell_count++;
+                    }
+
+                    if (colors_equal(pixels[next_index].color, fill_source_color))
+                    {
+                        current_index = next_index;
+                        dead_end_count = 0;
+                    }
+
+                    // Handle dead end
+                    if (!colors_equal(pixels[right_index].color, fill_source_color) &&
+                        !colors_equal(pixels[bottom_index].color, fill_source_color) &&
+                        !colors_equal(pixels[left_index].color, fill_source_color) &&
+                        !colors_equal(pixels[top_index].color, fill_source_color))
+                    {
+                        next_index = generate_next_index(current_index);
+
+                        if (colors_equal(pixels[next_index].color, selected_color))
+                        {
+                            // Stop on outer area with selected color
+                            if (!already_in_current_fill(next_index))
+                            {
+                                pixels[next_index].color = selected_color;
+                                dead_end_count = MAX_DEAD_ENDS;
+                            }
+                            else
+                            {
+                                current_index = next_index;
+                            }
+                        }
+
+                        dead_end_count++;
+                    }
+
+                    fill_current_index = current_index;
                 }
             }
+        }
+
+        // Erase multiple cells at once
+        if (fill_active && IsMouseButtonDown(MOUSE_BUTTON_RIGHT) && !colors_equal(fill_source_color, WHITE))
+        {
+            while (dead_end_count < MAX_DEAD_ENDS)
+            {
+                int current_index = fill_current_index;
+                int next_index = generate_next_index(current_index);
+
+                int right_index = current_index + 1;
+                int bottom_index = current_index + CANVAS_WIDTH;
+                int left_index = current_index - 1;
+                int top_index = current_index - CANVAS_WIDTH;
+
+                if (colors_equal(pixels[current_index].color, fill_source_color))
+                {
+                    pixels[current_index].color = WHITE;
+                    current_fill_cells[current_fill_cell_count] = current_index;
+                    current_fill_cell_count++;
+                }
+
+                if (colors_equal(pixels[next_index].color, fill_source_color))
+                {
+                    current_index = next_index;
+                    dead_end_count = 0;
+                }
+
+                // Handle dead end
+                if (!colors_equal(pixels[right_index].color, fill_source_color) &&
+                    !colors_equal(pixels[bottom_index].color, fill_source_color) &&
+                    !colors_equal(pixels[left_index].color, fill_source_color) &&
+                    !colors_equal(pixels[top_index].color, fill_source_color))
+                {
+                    next_index = generate_next_index(current_index);
+
+                    if (colors_equal(pixels[next_index].color, WHITE))
+                    {
+                        if (!already_in_current_fill(next_index))
+                        {
+                            pixels[next_index].color = WHITE;
+                            dead_end_count = MAX_DEAD_ENDS;
+                        }
+                        else
+                        {
+                            current_index = next_index;
+                        }
+                    }
+
+                    dead_end_count++;
+                }
+
+                fill_current_index = current_index;
+            }
+        }
+
+        Color fill_cursor_color = selected_color;
+
+        if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT))
+        {
+            fill_cursor_color = WHITE;
+        }
+
+        if (select_index != -1 || IsMouseButtonDown(MOUSE_BUTTON_RIGHT))
+        {
+            DrawRectangle(pos_x, pos_y, CELL_SIZE, CELL_SIZE, BLACK);
+            DrawRectangle(pos_x + CELL_SIZE / 4, pos_y + CELL_SIZE / 4, CELL_SIZE / 2, CELL_SIZE / 2, fill_cursor_color);
+            DrawRectangleLines(pos_x, pos_y, CELL_SIZE, CELL_SIZE, WHITE);
         }
     }
 }
@@ -427,7 +627,6 @@ int main()
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Pixel Art Editor");
 
     Pixel pixels[CELL_COUNT];
-    Pixel pixels_prev[CELL_COUNT];
 
     for (int i = 0; i < CELL_COUNT; i++)
     {
@@ -440,10 +639,6 @@ int main()
         pixels[i].pos_x = pos_x;
         pixels[i].pos_y = pos_y;
         pixels[i].color = WHITE;
-
-        pixels_prev[i].pos_x = pos_x;
-        pixels_prev[i].pos_y = pos_y;
-        pixels_prev[i].color = WHITE;
     }
 
     while (!WindowShouldClose())
@@ -457,8 +652,12 @@ int main()
         pick_color();
 
         cell_hover();
-        draw_pixel(&pixels[0]);
-        erase_pixel(&pixels[0]);
+
+        if (!fill_mode)
+        {
+            draw_pixel(&pixels[0]);
+            erase_pixel(&pixels[0]);
+        }
 
         undo(&pixels[0]);
         redo(&pixels[0]);
